ca69b4b95d52d3a7aa5f2291e4a60e37666c96d3
Vadim <vmakeev.dev@gmail.com>
2022-07-06 15:07:55 +0400

    Add validation

--- auth.php
+++ auth.php
@@ -4,57 +4,49 @@
     setcookie("password", null, time() - 2592000);
     $message = "";
     if(count($_POST)){
-    if(!$_POST['login'] or !$_POST['password']){
+    if(empty($_POST['login']) or empty($_POST['password'])){
         $message = "Заполните все поля";
     }
     else{
         $configs = include('config.php');
         $domain = $configs['domain'];
 
-        if(isset($_POST['login'])){
-            $login = $_POST['login'];
-            if ($login == ''){
-                unset($login);
-            }
-        }
+        $loginLength = strlen($_POST['login']);
 
-        if(isset($_POST['password'])){
-            $password = $_POST['password'];
-            if($password == ''){
-                unset($password);
-            }
-        }
-
-        if(empty($login) or empty($password))
-        {
-            exit("Вы ввели не всю инфу");
-        }
-
-        $login = trim($login);
-        $password = trim($password);
-
-        $db = include ("db.php");
-
-        $db_login = mysqli_real_escape_string($db, $login);
-
-        $result = mysqli_query($db, "SELECT * FROM users WHERE login=('$db_login')");
-
-
-        $myrow = mysqli_fetch_array($result);
-
-        if(empty($myrow['password']))
-        {
-            $message = ("Извините, введеный вами логин или пароль неверный");
+        if($loginLength > 20 or !(preg_match('/^([0-9a-zA-Z-_.]{3,20})$/', $_POST['login']))){
+            $message = "Введите не меньше 3 и не более 20 символов <br/> Используйте латиницу";
         }
         else{
-            if(password_verify($password, $myrow['password'])){
-                setcookie("login", $login, time() + 2592000);
-                setcookie("password", $myrow['password'], time() + 2592000);
-                header("Location: http://$domain/index.php");
-                exit();
+            $login = $_POST['login'];
+
+            $password = $_POST['password'];
+
+            $login = trim($login);
+            $password = trim($password);
+
+            $db = include ("db.php");
+
+            $db_login = mysqli_real_escape_string($db, $login);
+
+            $result = mysqli_query($db, "SELECT * FROM users WHERE login=('$db_login')");
+
+
+            $myrow = mysqli_fetch_array($result);
+
+            if(empty($myrow['password']))
+            {
+                $message = ("Извините, введеный вами логин или пароль неверный");
             }
             else{
-                $message = "Извините введеный вами пароль неверный";
+                if(password_verify($password, $myrow['password'])){
+                    setcookie("login", $login, time() + 2592000);
+                    setcookie("password", $myrow['password'], time() + 2592000);
+                    header("Location: http://$domain/index.php");
+                    exit();
+                }
+                else{
+                    $message = "Извините введеный вами пароль неверный";
+                }
             }
         }
     }
@@ -68,8 +60,8 @@
     <div class="cont">
         <form class="form" method="post">
             <h2>Авторизация</h2>
-            <input placeholder="login" name="login" type="text" maxlength="15" size="15"/>
-            <input placeholder="password" name="password" type="password" maxlength="15" size="15"/>
+            <input placeholder="login" name="login" type="text" maxlength="20" size="20"/>
+            <input placeholder="password" name="password" type="password" maxlength="20" size="20"/>
             <span class="message">
                 <?php echo $message; ?>
             </span>
--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
 $message = "";
 if(count($_POST)){
-    if (!$_POST['login'] or !$_POST['password']) {
+    if (empty($_POST['login']) or empty($_POST['password'])) {
         $message = 'Заполните поля';
     } else {
         $db = include ("db.php");
@@ -10,46 +10,39 @@ if(count($_POST)){
         if(!$db){
             die(('failed ' . mysqli_connect_error()));
         }
+        $loginLength = strlen($_POST['login']);
 
-        if(isset($_POST['login'])) {
+        if($loginLength > 20 or !(preg_match('/^([0-9a-zA-Z-_.]{3,20})$/', $_POST['login']))){
+            $message = "Введите не меньше 3 и не более 20 символов <br/> Используйте латиницу";
+        }
+        else{
             $login = $_POST['login'];
-            if($login == ''){
-                unset($login);
-            }
-        }
-
-        if(isset($_POST['password'])){
             $password = $_POST['password'];
-            if($password == ''){
-                unset($password);
-            }
-        }
 
-        $login = trim($login);
-        $password = trim($password);
-        $db_login = mysqli_real_escape_string($db, $login);
+            $login = trim($login);
+            $password = trim($password);
+            $db_login = mysqli_real_escape_string($db, $login);
 
-        $result = mysqli_query($db, "SELECT id FROM users WHERE login=('$db_login')");
+            $result = mysqli_query($db, "SELECT id FROM users WHERE login=('$db_login')");
 
-        $myrow = mysqli_fetch_array($result);
+            $myrow = mysqli_fetch_array($result);
 
-        if(!empty($myrow['id'])){
-            $message = "Логин занят";
-        } else {
-            $password = password_hash($password, PASSWORD_DEFAULT);
-            $result2 = mysqli_query($db,"INSERT INTO users (login, password) VALUES ('$db_login', '$password')");
-
-            if($result2 == 'TRUE'){
-                setcookie("login", $login, time() + 2592000);
-                setcookie("password", $password, time() + 2592000);
-                header("Location: http://$domain/index.php");
-                exit();
+            if(!empty($myrow['id'])){
+                $message = "Логин занят";
             } else {
-                echo "Ошибка hash!";
+                $password = password_hash($password, PASSWORD_DEFAULT);
+                $result2 = mysqli_query($db,"INSERT INTO users (login, password) VALUES ('$db_login', '$password')");
+
+                if($result2 == 'TRUE'){
+                    setcookie("login", $login, time() + 2592000);
+                    setcookie("password", $password, time() + 2592000);
+                    header("Location: http://$domain/index.php");
+                    exit();
+                } else {
+                    echo "Ошибка hash!";
+                }
             }
         }
-
-
     }
 }
 
@@ -62,8 +55,8 @@ if(count($_POST)){
     <div class="cont">
         <form class="form" method="post">
             <span class="title">Регистрация</span>
-            <input name="login" placeholder="Введите логин" type="text" />
-            <input name="password" placeholder="Введите пароль" type="password">
+            <input name="login" placeholder="Введите логин" type="text" maxlength="20" size="20" />
+            <input name="password" placeholder="Введите пароль" type="password" maxlength="20" size="20">
             <span class="message"><?php echo $message; ?></span>
             <input value="Зарегистрироваться" class="submit" type="submit">
             <span>
